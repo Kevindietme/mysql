87c1aecf5a9852d3d10dd6870365dd0ba451770b
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-06-12 22:13:51 -0400

    This is an additional practice. A movie table project of 5 movies including data and a cover image.

--- movie_table.sql
+++ movie_table.sql
@@ -1,24 +1,15 @@
-CREATE TABLE address(
+CREATE TABLE movies(
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-    person_id VARCHAR(6) NOT NULL, 
-
-    first_name VARCHAR(50),
-    middle_name VARCHAR(50),
-    last_name VARCHAR(50),
-
-    phone VARCHAR(20),
-    email VARCHAR(50),
-
-    address1 TINYTEXT,
-    address2 TINYTEXT,
-    city VARCHAR(50),
-    state VARCHAR(30),
-    zip INT(5),
-    country VARCHAR(50),
-
-    birthyear INT(4),
-    birthmonth INT(2),
-    birthdate INT(2),
-
-    created_at DATETIME
+    movie_id VARCHAR(15) NOT NULL, 
+    cover_image VARCHAR(10000),
+    title VARCHAR(25),
+    publish_year INT(4),
+    genre VARCHAR(25),
+    duration INT(4),
+    director VARCHAR(25),
+    main_actor VARCHAR(25),
+    total_revenue INT(10),
+    rating VARCHAR(6),
+    producer VARCHAR(30),
+    budget INT(10)
 );
---
+++ movie_value.sql
@@ -0,0 +1,79 @@
+INSERT INTO movies VALUES (
+    null,
+    'mov1',
+    'https://resizing.flixster.com/-wz8szqpqEtdTDH3qZBHi8OuaEU=/300x300/v2/https://flxt.tmsimg.com/assets/p16587_v_h9_ad.jpg',
+    'Major Payne',
+    '1995',
+    'Adventure Comedy',
+    '95',
+    'Nick Castle',
+    'Damian Wayans',
+    '7000000',
+    'PG-13',
+    'Eric Gold',
+    '5000000'
+);
+
+INSERT INTO movies VALUES (
+    null,
+    'mov2',
+    'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DBB71A31D7D865F2205FD2219283149AAA647A9A3947CAB4BDD35DCBB784F656/scale?width=1200&aspectRatio=1.78&format=jpeg',
+    'Bruce Almighty',
+    '2003',
+    'Comedy',
+    '105',
+    'Tom Shadyac',
+    'Jim Carry',
+    '67000000',
+    'PG-13',
+    'Tom Shadyac',
+    '81000000'
+);
+
+INSERT INTO movies VALUES (
+    null,
+    'mov3',
+    'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/3762af39f1a8cf529742dda67d31630e63e9e70d293647e589b68212021c19bd._RI_TTW_.jpg',
+    'How to Lose a Guy 10 Days',
+    '2003',
+    'Comedy Romance',
+    '205',
+    'Donald Petrie',
+    'Kate Hudson',
+    '23000000',
+    'PG-13',
+    'Lynda Obst',
+    '50000000'
+);
+
+INSERT INTO movies VALUES (
+    null,
+    'mov4',
+    'https://m.media-amazon.com/images/I/81TfISzuT6L._AC_UF894,1000_QL80_.jpg',
+    'Yours Mine and Ours',
+    '2005',
+    'Comedy Family',
+    '88',
+    'Raja Gosno',
+    'Dennis Quaid',
+    '17000000',
+    'PG',
+    'Richard Seuckle',
+    '45000000'
+);
+
+INSERT INTO movies VALUES (
+    null,
+    'mov5',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Poster_-_Gone_With_the_Wind_01.jpg/1200px-Poster_-_Gone_With_the_Wind_01.jpg',
+    'Gone With the Wind',
+    '1939',
+    'Drama Romance',
+    '240',
+    'Victor Flemming',
+    'Thomas Mitchell',
+    '1000000',
+    'PG',
+    'David Selznick',
+    '3000000'
+);
